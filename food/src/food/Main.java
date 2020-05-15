b1e4927945398cd16ff5603d30f3bac7d1bb7ac0
okamotonami <okamotonami@localhost>
2020-05-15 16:37:45 +0900

    add

---
+++ food/src/food/Main.java
@@ -0,0 +1,12 @@
+package food;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("好きな食べ物：みかん");
+		System.out.println("理由；食べやすい");
+
+	}
+
+}
